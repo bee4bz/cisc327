ce69a148068c4f80ebe5679857118266d3775c6c
Alex McKinnon <alex@mckinnon.io>
2018-11-01 21:29:46 -0400

    Fixed validate script
    fixed script validate

--- cmpe327_validate.sh
+++ cmpe327_validate.sh
@@ -4,10 +4,43 @@
 #
 #
 
-cd inputs
+for i in outputs/*
+do
 
-for i in *.txt do
-  echo "checking outputs of test".$i
-  diff ../outputs/$i.txt ../expected/$i.txt
-  diff ../outputs/$i.log ../expected/$i.log
+  echo "checking outputs of test"
+  stuff=sudo diff "outputs/${i:8}" "expected/${i:8}"
+  #cmp "outputs/${i:8}" "expected/${i:8}"
+
+#echo $stuff
+
+  if [ -n "$stuff" ];
+  then
+
+    echo "no"
+
+  else
+
+    echo "!="
+
+    echo "outputs/${i:8}"
+    echo "Not a Match"
+
+    echo "       "
+    #cat outputs/${i:8}
+    #cat expected/${i:8}
+    echo "-----------" >>"results/result${i:8}.txt"
+    echo "outputs/${i:8}">>"results/result${i:8}.txt"
+    echo "!="
+    echo "!=" >>"results/result${i:8}.txt"
+    echo "expected/${i:8}">>"results/result${i:8}.txt"
+    echo "         " >>"results/result${i:8}.txt"
+
+
+  fi
+
+#echo "      "
+#echo "outputs/${i:8}"
+#echo "      "
+#echo "expected/${i:8}"
+
 done
